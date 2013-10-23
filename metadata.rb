d792a509ace7bb6256bf61e3ec70807d1a002980
Christopher Maier <cm@opscode.com>
2013-10-23 11:52:41 -0400

    Bump to version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'enterprise'
 license          'All rights reserved'
 description      'Installs common libraries and resources for Enterprise Chef and closed-source additions'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends          'runit', '= 1.3.0'
