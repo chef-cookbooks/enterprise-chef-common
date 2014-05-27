d183021687198c836ee5d793210638d28fa5258a
James Casey <james@opscode.com>
2014-05-27 12:50:27 -0700

    svdir_line has replacements, so needs double-quotes

--- recipes/runit_sysvinit.rb
+++ recipes/runit_sysvinit.rb
@@ -9,7 +9,7 @@ install_path = node[project_name]['install_path']
 init_id = node[project_name]['sysvinit_id'] || 'SV'
 
 # We assume you are sysvinit
-svdir_line = '#{init_id}:123456:respawn:#{install_path}/embedded/bin/runsvdir-start'
+svdir_line = "#{init_id}:123456:respawn:#{install_path}/embedded/bin/runsvdir-start"
 execute "echo '#{svdir_line}' >> /etc/inittab" do
   not_if "grep '#{svdir_line}' /etc/inittab"
   notifies :run, "execute[init q]", :immediately
