4d4db166129c85df0dff4d706410c52f8ec966ba
Irving Popovetsky <irving@getchef.com>
2014-07-18 11:24:20 -0700

    Bump version to 0.4.3 and update changelog

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'enterprise'
 license          'All rights reserved'
 description      'Installs common libraries and resources for Enterprise Chef and closed-source additions'
 long_description 'Installs common libraries and resources for Enterprise Chef and closed-source additions'
-version          '0.4.2'
+version          '0.4.3'
 
 depends          'runit', '> 1.0.0'
