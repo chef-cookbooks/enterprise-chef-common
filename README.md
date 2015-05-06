99af42a9359e677bae81cbe3879c17e7de5e5012
Nathan L Smith <smith@chef.io>
2015-05-06 11:42:21 -0500

    Testing updates to work with ChefDK
    * Reorder the README to put the license at the end
    * Change names of projects it works with
    * Remove Gemfile and assume ChefDK
    * Update specs to be compatible with new ChefDK/RSpec/ChefSpec

--- README.md
+++ README.md
@@ -1,29 +1,8 @@
 enterprise cookbook
 ===================
 
-## License
-
-All files in the repository are licensed under the Apache 2.0 license. If any
-file is missing the License header it should assume the following is attached;
-
-```
-Copyright 2014 Chef Software Inc
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-```
-
-This cookbook provides common functionality needed for Enterprise
-Chef, Pushy, Reporting, and other enterprise-grade Omnibus projects.
+This cookbook provides common functionality needed for Chef server,
+Push jobs, Reporting, and other enterprise-grade Omnibus projects.
 
 Your omnibus project attributes should define the following attributes:
 
@@ -48,4 +27,28 @@ In addition, you need to have:
 
 ## Testing
 
-Run `bundle exec rspec` (after running `bundle install`) to run ChefSpec examples.
+[ChefDK](http://downloads.chef.io/chef-dk/) must be installed (version 0.5.1 at
+the time of this writing.)
+
+Run `chef exec rspec` to run ChefSpec examples.
+
+## License
+
+All files in the repository are licensed under the Apache 2.0 license. If any
+file is missing the License header it should assume the following is attached;
+
+```
+Copyright 2014-2015 Chef Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
--- spec/recipes/runit_spec.rb
+++ spec/recipes/runit_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'enterprise::runit' do
-  let(:runner) { ChefSpec::Runner.new }
+  let(:runner) { ChefSpec::SoloRunner.new }
   subject(:chef_run) { runner.converge(described_recipe) }
 
   before :each do
     # ChefSpec told me to do this
-    stub_command("grep 'SV:123456:respawn:/opt/opscode/embedded/bin/runsvdir-start' /etc/inittab")
+    stub_command("grep 'SV:123456:respawn:/embedded/bin/runsvdir-start' /etc/inittab")
     stub_command('initctl status opscode-runsvdir | grep start')
     stub_command('initctl status private-chef-runsvdir | grep stop')
     stub_command('initctl status testproject-runsvdir | grep stop')
@@ -18,7 +18,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on Amazon Linux' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'amazon', version: '2014.03') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'amazon', version: '2014.03') }
 
     it 'includes the runit_upstart recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_upstart'
@@ -26,7 +26,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on Debian' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'debian', version: '7.4') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'debian', version: '7.4') }
 
     it 'includes the runit_upstart recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_upstart'
@@ -34,7 +34,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on Fedora do' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'fedora', version: '20') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'fedora', version: '20') }
 
     it 'includes the runit_upstart recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_upstart'
@@ -42,7 +42,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on RHEL 5' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'redhat', version: '5.9') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'redhat', version: '5.9') }
 
     it 'includes the runit_sysvinit recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_sysvinit'
@@ -50,7 +50,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on RHEL 6' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'redhat', version: '6.5') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'redhat', version: '6.5') }
 
     it 'includes the runit_upstart recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_upstart'
@@ -58,7 +58,7 @@ describe 'enterprise::runit' do
   end
 
   context 'when on Ubuntu' do
-    let(:runner) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') }
+    let(:runner) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04') }
 
     it 'includes the runit_upstart recipe' do
       expect(chef_run).to include_recipe 'enterprise::runit_upstart'
