9509f696507d095aeef12a4d8e94ae68037ecb89
Nathan L Smith <smith@chef.io>
2015-05-12 22:11:56 -0500

    use sysvinit id

--- recipes/runit.rb
+++ recipes/runit.rb
@@ -26,7 +26,8 @@ node.set['runit']['sv_dir']       = "#{install_path}/sv"
 node.set['runit']['lsb_init_dir'] = "#{install_path}/init"
 
 component_runit_supervisor node['enterprise']['name'] do
-  install_path node[node['enterprise']['name']]['install_path']
   ctl_name node[node['enterprise']['name']]['ctl_name'] ||
     "#{node['enterprise']['name']}-ctl"
+  init_id node[node['enterprise']['name']]['sysvinit_id']
+  install_path node[node['enterprise']['name']]['install_path']
 end
