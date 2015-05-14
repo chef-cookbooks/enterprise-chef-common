6a26c70b2109a9a6696b737dadc0520fbfc1931b
Nathan L Smith <smith@chef.io>
2015-05-14 12:09:00 -0500

    add matcher for supervisor service

--- spec/recipes/runit_spec.rb
+++ spec/recipes/runit_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe 'enterprise::runit' do
-  let(:runner) do
-    ChefSpec::SoloRunner.new
-  end
-  subject(:chef_run) { runner.converge(described_recipe) }
-  let(:enterprise_name) { "testproject" }
-
-  before :each do
-    # Set the node project_name
-    runner.node.set['enterprise']['name'] = enterprise_name
-    runner.node.set['testproject']['install_path'] = '/opt/testproject'
+  subject(:chef_run) do
+    ChefSpec::SoloRunner.new do |node|
+      node.set['enterprise']['name'] = "testproject"
+      node.set['testproject']['install_path'] = "/opt/testproject"
+      node.set['testproject']['sysvinit_id'] = "TP"
+    end.converge(described_recipe)
   end
 
-  it "creates a component_runit_supervisor"
+  it "creates a component_runit_supervisor" do
+    expect(chef_run).to create_component_runit_supervisor("testproject").with(
+      :ctl_name => "testproject-ctl",
+      :init_id => "TP",
+      :install_path => "/opt/testproject",
+    )
+  end
 end
