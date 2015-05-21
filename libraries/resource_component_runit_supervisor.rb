b8d0dd148e3f5a31ad8722efc9602000c6a1aa38
Nathan L Smith <smith@chef.io>
2015-05-20 22:49:24 -0500

    make more supervisor resources required

--- libraries/resource_component_runit_supervisor.rb
+++ libraries/resource_component_runit_supervisor.rb
@@ -7,10 +7,10 @@ class Chef
       actions :create, :delete
       default_action :create
 
-      attribute :name, :kind_of => String, :name_attribute => true
-      attribute :ctl_name, :kind_of => String
+      attribute :name, :kind_of => String, :name_attribute => true, :required => true
+      attribute :ctl_name, :kind_of => String, :required => true
       attribute :sysvinit_id, :kind_of => String, :default => "SV"
-      attribute :install_path, :kind_of => String
+      attribute :install_path, :kind_of => String, :required => true
     end
   end
 end
