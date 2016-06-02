79f86992238c6d32d7f6cbb9ed73dc25fe3e0d77
Yvonne Lam <yvonne@chef.io>
2016-06-02 14:37:28 -0700

    Verify services running under systemd on ubuntu 16.04.

--- test/integration/component_runit_supervisor_create/serverspec/systemd_spec.rb
+++ test/integration/component_runit_supervisor_create/serverspec/systemd_spec.rb
@@ -1,7 +1,8 @@
 require "spec_helper"
 
-if os[:family] == "redhat" && os[:release].to_i == 7
+if (os[:family] == "redhat" && os[:release].to_i == 7) ||
+   (os[:family] == "ubuntu" && os[:release] == "16.04")
   describe service("testproject-runsvdir-start.service") do
-    it { should be_running }
+    it { should be_running.under('systemd') }
   end
 end
