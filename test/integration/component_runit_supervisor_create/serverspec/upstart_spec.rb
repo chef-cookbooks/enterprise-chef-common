b014a69b17e5597c2fe84c9ca86ca1b09590c3a5
Ryan Cragun <me@ryan.ec>
2017-07-07 12:31:36 -0700

    [CLOUD-429] Fix runsvdir reboot issues with Upstart
    * Remove duplicate Ubuntu 14.04 kitchen platform
    * Update gem dependencies
    * Increase the Upstart kill timeout for runsvdir from 5 seconds to 30. Without
      this it was possible for Upstart to kill the runsvdir process before
      the children runsv processes had been terminated, leaving them orphaned
      to Upstart.
    * Send `sv force-stop` to runsv processes before runsvdir is terminated.
      This ensures that processes that don't exit after 7 seconds are terminated.
    * Transition the Upstart goal to 'stop' on reboot. This fixes a bug where
      Upstart would respawn runsvdir during a reboot, causing services to
      stop and start when the system is going down.
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- test/integration/component_runit_supervisor_create/serverspec/upstart_spec.rb
+++ test/integration/component_runit_supervisor_create/serverspec/upstart_spec.rb
@@ -5,4 +5,13 @@ if (os[:family] == 'redhat' && os[:release].to_i == 6) ||
   describe command('initctl status testproject-runsvdir') do
     its(:stdout) { should match(/start\/running/) }
   end
+
+  describe file('/etc/init/testproject-runsvdir.conf') do
+    # Increases the kill timeout from 5 seconds
+    its(:content) { is_expected.to match(/kill timeout/) }
+    # Sends SIGKILL to timed out runsv's
+    its(:content) { is_expected.to match(/sv force-stop/) }
+    # Shuts down on reboots
+    its(:content) { is_expected.to match(/stop on runlevel \[016\]/) }
+  end
 end
