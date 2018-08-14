d9e32721fe7cf7aa5e119e3ef456b6d2ef2bca64
Ryan Hass <rhass@users.noreply.github.com>
2018-08-14 10:41:18 -0700

    {WIP} Update resource to work with private-chef
    This is related to the work in this private-chef cookbook on the
    following branch:
    https://github.com/chef/chef-server/tree/rhass/update-private-chef-cb-for-custom-resource/omnibus/files/private-chef-cookbooks
    
    The intent is to be able to update to the latest version of the runit
    cookbook, and replace the definition with a custom resource.
    
    Signed-off-by: Ryan Hass <rhass@users.noreply.github.com>

--- resources/component_runit_service.rb
+++ resources/component_runit_service.rb
@@ -11,7 +11,8 @@ property :svlogd_size, Integer
 property :svlogd_num, Integer
 property :ha, [TrueClass, FalseClass]
 property :control, Array
-property :runit_attributes, Hash
+property :runit_attributes, Hash, default: {}
+property :start_down, [TrueClass, FalseClass], default: false
 
 action :enable do
   package = new_resource.package
@@ -47,6 +48,25 @@ action :enable do
     new_resource.runit_attributes.each do |attr_name, attr_value|
       send(attr_name.to_sym, attr_value)
     end
+    start_down new_resource.start_down
+  end
+
+  # The runit cookbook fixed an issue with the sv binary being symlinked to
+  # the init.d on directory on Debian/Ubuntu which causes chef-server-ctl to get
+  # angry when we attempt to start the service manually. This template preserves
+  # the system environment PATH variable rather than hard-coding the value in the
+  # init script.
+  if component == 'rabbitmq'
+    edit_resource!(:template, '/opt/opscode/init/rabbitmq') do
+      source 'sv-rabbitmq-init.erb'
+      cookbook 'private-chef'
+      variables({
+        :name => 'rabbitmq',
+        :sv_bin => '/opt/opscode/embedded/bin/sv',
+        :init_dir => '/opt/opscode/init/'
+      })
+      only_if { platform_family? 'debian' }
+    end
   end
 
   # Keepalive management
@@ -75,5 +95,73 @@ end
 action :down do
   log "stop runit_service[#{new_resource.name}]" do
     notifies :down, "runit_service[#{new_resource.name}]", :immediately
+  end
+end
+
+action :restart do
+  runit_service new_resource.name do
+    action :restart
+  end
+end
+
+# Copy-pasta from enable resource for dev work to upgrade runit cookbook
+# If this is to be used at any point going forward, the logic within should
+# be converted into helper methods called by the enable and create actions.
+action :create do
+  package = new_resource.package
+  component = new_resource.name
+  log_directory = new_resource.log_directory || node[package][component]['log_directory']
+
+  # runit resources don't support reloading the log service as an action :(
+  execute "restart_#{component}_log_service" do
+    command "#{node['runit']['sv_bin']} restart #{node['runit']['sv_dir']}/#{component}/log"
+    action :nothing
+  end
+
+  template "#{log_directory}/config" do
+    source 'config.svlogd'
+    cookbook 'enterprise'
+    mode '0644'
+    owner 'root'
+    group 'root'
+    notifies :run, "execute[restart_#{component}_log_service]"
+    variables(
+      svlogd_size: (new_resource.svlogd_size || node[package][component]['log_rotation']['file_maxbytes']),
+      svlogd_num: (new_resource.svlogd_num || node[package][component]['log_rotation']['num_to_keep'])
+    )
+  end
+
+  runit_service component do
+    action :create
+    retries 20
+    control new_resource.control if new_resource.control
+    options(
+      log_directory: log_directory
+    )
+    new_resource.runit_attributes.each do |attr_name, attr_value|
+      send(attr_name.to_sym, attr_value)
+    end
+  end
+
+  # Keepalive management
+  #
+  # Our keepalived setup knows which services it must manage by
+  # looking for a 'keepalive_me' sentinel file in the service's
+  # directory.
+  if EnterpriseChef::Helpers.ha?(node)
+    # We need special handling for the ha param, as it's a boolean and
+    # could be false, so we explicitly check for nil?
+    is_keepalive_service = if new_resource.ha.nil?
+                             node[package][component]['ha']
+                           else
+                             new_resource.ha
+                           end
+    file "#{node['runit']['sv_dir']}/#{component}/keepalive_me" do
+      action is_keepalive_service ? :create : :delete
+    end
+
+    file "#{node['runit']['sv_dir']}/#{component}/down" do
+      action is_keepalive_service ? :create : :delete
+    end
   end
 end
