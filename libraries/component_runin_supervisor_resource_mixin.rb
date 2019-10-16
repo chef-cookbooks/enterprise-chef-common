8c21a175088af42feb561eda177b26c435496f2e
Tim Smith <tsmith84@gmail.com>
2019-10-16 12:11:26 -0700

    Remove the name property we don't need
    These come out of the box
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/component_runin_supervisor_resource_mixin.rb
+++ libraries/component_runin_supervisor_resource_mixin.rb
@@ -5,7 +5,6 @@ module ComponentRunitSupervisorResourceMixin
       actions :create, :delete
       default_action :create
 
-      property :name, kind_of: String, name_property: true, required: true
       property :ctl_name, kind_of: String, required: true
       property :sysvinit_id, kind_of: String, default: 'SV'
       property :install_path, kind_of: String, required: true
