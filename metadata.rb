24d0c60a9ba9e5973d0936b6be3b238b79ba7478
Prajakta Purohit <prajakta@chef.io>
2020-06-30 18:02:03 -0700

    Update runit to 5.1.6 and bump the enterprise-chef-common cookobook to 0.15.3
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs common libraries and resources for Chef server and add-ons'
-version '0.15.2'
+version '0.15.3'
 
-depends 'runit', '= 5.1.1'
+depends 'runit', '= 5.1.6'
 
 source_url 'https://github.com/chef-cookbooks/enterprise-chef-common'
 issues_url 'https://github.com/chef-cookbooks/enterprise-chef-common/issues'
